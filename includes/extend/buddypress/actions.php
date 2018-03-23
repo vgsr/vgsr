2032d986109820cf35a6fda870356494c533585c
Laurens Offereins <lmoffereins@gmail.com>
2018-03-23 18:14:37 +0100

    BuddyPress: disable dedicated member type members directory pages
    Member types for VGSR are not listed separately, but in their own tab
    alongside the default members directory tabs.
    
    Disabling is achieved by always returning false on the current member
    type, which is only truthy when on a members directory page. Additionally
    the member type members directory permalink is forcefully false, so no
    linking to the directory pages can exist from BP's own functions.

--- includes/extend/buddypress/actions.php
+++ includes/extend/buddypress/actions.php
@@ -18,12 +18,14 @@ add_filter( 'bp_activity_custom_post_type_comment_action',      'vgsr_bp_activit
 add_filter( 'bp_blogs_format_activity_action_new_blog_comment', 'vgsr_bp_activity_post_type_comment_action', 10, 2 );
 
 // Members
-add_action( 'bp_members_directory_member_types', 'vgsr_bp_members_directory_tabs'            );
-add_action( 'bp_before_directory_members_tabs',  'vgsr_bp_add_member_count_filter',    99    );
-add_action( 'bp_members_directory_member_types', 'vgsr_bp_remove_member_count_filter',  0    );
-add_filter( 'bp_legacy_theme_ajax_querystring',  'vgsr_bp_legacy_ajax_querystring',    10, 7 );
-add_filter( 'bp_members_pagination_count',       'vgsr_bp_members_pagination_count'          );
-add_action( 'bp_member_header_actions',          'vgsr_bp_add_member_header_actions'         );
+add_action( 'bp_members_directory_member_types',      'vgsr_bp_members_directory_tabs'            );
+add_action( 'bp_before_directory_members_tabs',       'vgsr_bp_add_member_count_filter',    99    );
+add_action( 'bp_members_directory_member_types',      'vgsr_bp_remove_member_count_filter',  0    );
+add_filter( 'bp_legacy_theme_ajax_querystring',       'vgsr_bp_legacy_ajax_querystring',    10, 7 );
+add_filter( 'bp_members_pagination_count',            'vgsr_bp_members_pagination_count'          );
+add_filter( 'bp_get_current_member_type',             '__return_false'                            );
+add_filter( 'bp_get_member_type_directory_permalink', '__return_false'                            );
+add_action( 'bp_member_header_actions',               'vgsr_bp_add_member_header_actions'         );
 
 // Plugin Settings
 add_filter( 'vgsr_admin_get_settings_sections', 'vgsr_bp_settings_sections' );
